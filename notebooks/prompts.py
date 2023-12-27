8d3426e20361569f60192b16353d6359cb2e6210
lfiebig <fiebiglennart@googlemail.com>
2023-12-27 17:27:17 +0100

    pipeline done for Zero shot and few shot prompt

--- notebooks/prompts.py
+++ notebooks/prompts.py
@@ -1,10 +1,373 @@
 from langchain.prompts import PromptTemplate
 
-CREATION_PROMPT = PromptTemplate(
+CREATION_PROMPT_ZS = PromptTemplate(
     input_variables=["summary"],
-    template= """create the HTML code for a Mobilepage, 
-    with a bit of styling, that represents following functionality: 
+    template= """Given a summary of a mobile screen, create the corresponding HTML code
+    with a bit of styling, that represents the described functionalities: 
     {summary}."""
+)
+CREATION_PROMPT_FS = PromptTemplate(
+    input_variables=["summary"],
+    template="""
+        Q: 
+        Given a summary of a mobile screen, create the corresponding HTML code with a bit of styling, that represents the described functionalities: 
+
+        The Screen displays different workout options to set, alongside the feature to start the workout.
+
+        A: 
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <style>
+        body {{
+            font-family: 'Arial', sans-serif;
+            background: #FFF;
+            margin: 0;
+            padding: 0;
+        }}
+
+        .navbar {{
+            background-color: #4CAF50; 
+            color: #FFF;
+            display: flex;
+            justify-content: space-between;
+            padding: 10px 20px;
+            align-items: center;
+        }}
+
+        .navbar h1 {{
+            margin: 0;
+            font-size: 24px;
+        }}
+
+        .navbar button {{
+            background: none;
+            border: none;
+            color: white;
+            font-size: 24px;
+            cursor: pointer;
+            padding: 0;
+            margin: 0 5px;
+        }}
+
+        .button-start {{
+            background: none;
+            border: 2px solid #4CAF50;
+            border-radius: 50%;
+            color: #4CAF50;
+            padding: 20px;
+            text-align: center;
+            margin: 20px auto;
+            display: block;
+            width: 200px;
+            font-size: 18px;
+            cursor: pointer;
+        }}
+
+        .menu-item {{
+            border-bottom: 1px solid #ccc;
+            display: flex;
+            justify-content: space-between;
+            padding: 10px 20px;
+            align-items: center;
+            color: #4CAF50;
+        }}
+
+        .menu-item:last-child {{
+            border-bottom: none;
+        }}
+
+        .menu-item button {{
+            background: none;
+            border: none;
+            color: inherit;
+            padding: 0;
+            cursor: pointer;
+            text-align: left;
+            width: 100%;
+            display: flex;
+            justify-content: space-between;
+        }}
+
+        .footer-nav {{
+            background-color: #4CAF50;
+            display: flex;
+            justify-content: space-around;
+            padding: 10px 0;
+            text-align: center;
+        }}
+
+        .nav-button {{
+            background: none;
+            border: none;
+            color: white;
+            padding: 10px;
+            font-size: 14px;
+            cursor: pointer;
+        }}
+
+        </style>
+        </head>
+        <body>
+
+        <div class="navbar">
+        <h1>Workout</h1>
+        <div>
+            <button class="info-button" aria-label="Information">i</button>
+            <button class="more-options-button" aria-label="More options">&#8942;</button>
+        </div>
+        </div>
+
+        <button class="button-start">
+        START WORKOUT
+        </button>
+
+        <div class="menu">
+        <div class="menu-item">
+            <button>
+            <span>Workout</span>
+            <span id="workout-selection">Full Body</span>
+            </button>
+        </div>
+        <div class="menu-item">
+            <button>
+            <span>Circuits</span>
+            <span id="circuits-selection">1</span>
+            </button>
+        </div>
+        <div class="menu-item">
+            <button>
+            <span>Instructor</span>
+            <span id="instructor-selection">Announcer</span>
+            </button>
+        </div>
+        </div>
+
+        <nav class="footer-nav">
+        <button class="nav-button">Workout</button>
+        <button class="nav-button">Learn</button>
+        <button class="nav-button">Achievements</button>
+        <button class="nav-button">Track</button>
+        </nav>
+
+        </body>
+        </html>
+
+        Q: 
+        Given a summary of a mobile screen, create the corresponding HTML code with a bit of styling, that represents the described functionalities: 
+
+        The mobile screen is part of a language translator application that asks the user to translate a given text sentence. 
+
+        A:
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Language Translation Practice</title>
+        <style>
+        body {{
+            font-family: 'Arial', sans-serif;
+            margin: 0;
+            padding: 0;
+            background-color: #f4f4f4;
+        }}
+
+        .container {{
+            max-width: 400px;
+            margin: auto;
+            background: #fff;
+            border-radius: 10px;
+            box-shadow: 0 2px 4px rgba(0,0,0,0.1);
+            padding: 20px;
+            margin-top: 20px;
+        }}
+
+        .progress-bar {{
+            height: 5px;
+            background-color: #4CAF50;
+            border-radius: 5px;
+            margin-bottom: 20px;
+        }}
+
+        .title {{
+            text-align: center;
+            font-size: 1.2em;
+            margin-bottom: 20px;
+        }}
+
+        .translation-section {{
+            display: flex;
+            align-items: center;
+            margin-bottom: 20px;
+        }}
+
+        .audio-button {{
+            background-color: #eee;
+            border: none;
+            padding: 10px;
+            margin-right: 10px;
+            border-radius: 50%;
+            cursor: pointer;
+        }}
+
+        .sentence {{
+            flex-grow: 1;
+        }}
+
+        .input-field {{
+            width: calc(100% - 20px);
+            padding: 10px;
+            margin-bottom: 20px;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+        }}
+
+        .buttons-container {{
+            width: 100%;
+            padding: 0;
+            display: flex;
+            justify-content: space-between;
+            margin-bottom: 20px; /* If needed */
+        }}
+
+        .button {{
+            width: 48%;
+            padding: 10px;
+            border: none;
+            border-radius: 5px;
+            cursor: pointer;
+        }}
+
+        .skip-button {{
+            background-color: #f44336;
+            color: white;
+        }}
+
+        .check-button {{
+            background-color: #4CAF50;
+            color: white;
+        }}
+        </style>
+        </head>
+        <body>
+        <div class="container">
+        <div class="progress-bar" style="width: 25%;"></div>
+        <div class="title">Translate this sentence</div>
+        <div class="translation-section">
+            <button class="audio-button">
+            &#9658;
+            </button>
+            <div class="sentence">Mis animales, mi tortuga, mi cangrejo.</div>
+        </div>
+        <input class="input-field" type="text" placeholder="Type the English translation">
+        <div class="buttons-container">
+            <button class="button skip-button">Skip</button>
+            <button class="button check-button">Check</button>
+        </div>
+        </div>
+
+        </body>
+        </html>
+
+        Q: 
+        Given a summary of a mobile screen, create the corresponding HTML code with a bit of styling, that represents the described functionalities: 
+
+        The mobile screen is a display page in a weather forecast app that shows various options.
+
+
+        A: 
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Weather App</title>
+        <style>
+            body, button {{
+                font-family: 'Arial', sans-serif;
+                margin: 0;
+                padding: 0;
+                background-color: #e9ecef;
+                color: #343a40;
+            }}
+
+            #app-container {{
+                padding: 20px;
+            }}
+
+            h2, h3 {{
+                margin-bottom: 16px;
+            }}
+
+            button {{
+                background-color: #f8f9fa;
+                border: 1px solid #ced4da;
+                color: #343a40;
+                padding: 8px;
+                margin-bottom: 8px;
+                width: calc(100% - 16px);
+                border-radius: 4px;
+                font-size: 16px;
+                cursor: pointer;
+            }}
+
+            button:hover {{
+                background-color: #e2e6ea;
+            }}
+
+            #temperature-toggle {{
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                gap: 8px;
+                margin-top: 16px;
+            }}
+
+            input[type="radio"] {{
+                cursor: pointer;
+            }}
+
+            label {{
+                font-size: 16px;
+                cursor: pointer;
+            }}
+
+        </style>
+        </head>
+        <body>
+            <div id="app-container">
+                <main>
+                    <section id="settings">
+                        <h2>SETTINGS</h2>
+                        <div id="saved-locations">
+                            <h3>SAVED LOCATIONS</h3>
+                            <button>New Location</button>
+                        </div>
+                        <div id="forecast-tabs">
+                            <h3>FORECAST TABS</h3>
+                            <button>7 Day Forecast</button>
+                            <button>Hourly Forecast</button>
+                            <button>Weather Details</button>
+                        </div>
+                        <div id="temperature-toggle">
+                            <input type="radio" id="fahrenheit" name="temperature" value="fahrenheit" checked>
+                            <label for="fahrenheit">F°</label>
+                            <input type="radio" id="celsius" name="temperature" value="celsius">
+                            <label for="celsius">C°</label>
+                        </div>
+                    </section>
+                </main>
+            </div>
+        </body>
+        </html>
+
+        Q: Given a summary of a mobile screen, create the corresponding HTML code with a bit of styling, that represents the described functionalities: 
+
+        {summary}
+
+        A:"""
 )
 
 SUMMARY_PROMPT = PromptTemplate(
@@ -15,45 +378,6 @@ SUMMARY_PROMPT = PromptTemplate(
         Extract the provided information of the screen and formulate a new description"""
 )
 
-SUMMARY_PROMPT_COT = PromptTemplate(
-    input_variables= ["summaries"],
-    template="""
-    Q: 
-    Please provide a short description of the mobile page based on the following five summaries. Functionalities, that are only provided by one of the five summaries should be ignored.
-
-    Summary 1: display image of a recipes app
-    Summary 2: list of ingredients for a recipe in a recipe app
-    Summary 3: page displaying information about recipes of specific dish
-    Summary 4: page displaying list of items required for a dish
-    Summary 5: page showing different recipes available.
-
-    Let's think step by step!
-
-    A:
-    Certainly, let's break it down step by step:
-
-    Summary 1: "Display image of a recipes app" - This suggests that it's a screen within a recipes app.
-
-    Summary 2: "List of ingredients for a recipe in a recipe app" - This indicates that the screen shows a list of ingredients for a specific recipe within the app.
-
-    Summary 3: "Page displaying information about recipes of specific dish" - This implies that the screen provides information about a particular dish or recipe.
-
-    Summary 4: "Page displaying list of items required for a dish" - Similar to Summary 2, it mentions a list of items (ingredients) required for a dish.
-
-    Summary 5: "Page showing different recipes available" - This suggests that the screen displays a collection of various recipes. This Summary is the only summary, mentioning multiple recipes, so we ignore this information.
-
-    From these summaries, ignoring summary 5, we can formulate a new description:
-
-    The screen is part of a recipe app and serves as a dedicated page displaying information about a specific dish, including a list of ingredients required for that recipe.
-
-    Q: 
-    Please provide a short description of the mobile page based on the following five summaries. Functionalities, that are only provided by one of the five summaries should be ignored.
-
-    {summaries}
-
-    A: Let's think step by step!"""
-)
-
 SUMMARY_PROMPT_FS = PromptTemplate(
     input_variables= ["summaries"],
     template="""
@@ -62,22 +386,19 @@ SUMMARY_PROMPT_FS = PromptTemplate(
         Summary 1: page displaying different workout options to set
         Summary 2: page displaying to start exercise
         Summary 3: page displaying to start workout
-        Summary 4: pop-up displaying about body work outs
-        Summary 5: timer and different features of a fitness app are displaying.
                 
-        Extract the provided information of the screen and formulate a new description. Exclude information from summaries, that are contradicting to the majority summaries.
+        Extract the provided information of the screen and formulate a new description.
 
-        A: The Screen is part of a fitness app and displays different workout options to set, alongside the feature to start the workout.
+        A: The Screen displays different workout options to set, alongside the feature to start the workout.
 
         Q: Given a collection of Screensummaries, which descripe the same screen:
 
         Summary 1: app asking to translate the given sentence
         Summary 2: page displaying about translating the sentence
-        Summary 3: page displaying the translation
         Summary 4: page to translate the text
         Summary 5: screen page of language translator application.
                 
-        Extract the provided information of the screen and formulate a new description. Exclude information from summaries, that are contradicting to the majority summaries.
+        Extract the provided information of the screen and formulate a new description.
 
         A: The mobile screen is part of a language translator application that asks the user to translate a given text sentence. 
 
@@ -89,46 +410,17 @@ SUMMARY_PROMPT_FS = PromptTemplate(
         Summary 4: setting page displaying various options in weather application.
         Summary 5: settings page displaying various options in weather application.
                 
-        Extract the provided information of the screen and formulate a new description. Exclude information from summaries, that are contradicting to the majority summaries.
+        Extract the provided information of the screen and formulate a new description.
 
-        A: The mobile screen is a display page in a weather forecast app that shows various options. It is a settings page where users can access and adjust different options in the weather application.
+        A: The mobile screen is a display page in a weather forecast app that shows various options.
 
         Q: Given a collection of Screensummaries, which descripe the same screen:
 
         {summaries}
                 
-        Extract the provided information of the screen and formulate a new description. Exclude information from summaries, that are contradicting to the majority summaries.
+        Extract the provided information of the screen and formulate a new description.
 
         A: 
         """
         )
 
-COT_CREATION_PROMPT = PromptTemplate(
-    input_variables=["summary"],
-    template= """create the HTML code for a Mobilepage, 
-    with a bit of styling, that represents following functionality: 
-    {summary}.
-    Let's think step by step."""
-)
-
-TOT_CREATION_PROMPT = PromptTemplate(
-    input_variables=["summary"],
-    template="""Imagine three different experts are solving this task.
-    All experts will write down 1 step of their thinking,
-    then share it with the group.
-    Then all experts will go on to the next step, etc.
-    If any expert realises they're wrong at any point then they leave.
-    The task is: 
-    create the HTML code for a Mobilepage, 
-    with a bit of styling, that represents following functionality: 
-    {summary}. 
-    provide me with the html code, all three experts agreed on"""
-)
-
-
-FEW_SHOT_CREATION_PROMPT = PromptTemplate(
-    input_variables=["summary"],
-    template="""Q: create the HTML code for a Mobilepage, 
-    with a bit of styling, that represents following functionality: 
-    """
-)
